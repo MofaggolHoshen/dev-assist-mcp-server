9d512a16906e6037f168307fedf68bee667db403
Mofaggol Hoshen <Mofaggol.Hoshen@outlook.com>
2026-04-29 23:20:53 +0200

    feat: add unit test for pingTool to verify 'pong' response

---
+++ tests/ping.test.ts
@@ -0,0 +1,8 @@
+import { pingTool } from "../src/tools/ping";
+
+describe("pingTool", () => {
+  it("should return pong", async () => {
+    const result = await pingTool.execute({});
+    expect(result).toEqual({ content: [{ type: "text", text: "pong" }] });
+  });
+});
